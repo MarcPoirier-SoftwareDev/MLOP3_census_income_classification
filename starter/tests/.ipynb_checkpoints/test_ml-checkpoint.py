92fe06b448b053e5c3eccdda05e43f4ff92ec629
Marc <marc.poirier37@gmail.com>
2025-07-23 06:15:33 +0200

    revert some change for debugging

--- starter/tests/.ipynb_checkpoints/test_ml-checkpoint.py
+++ starter/tests/.ipynb_checkpoints/test_ml-checkpoint.py
@@ -5,14 +5,23 @@ from ..ml.model import build_mlp
 from ..ml.model import Mlp
 
 
-@pytest.mark.parametrize('n_layers, hidden_size, dropout_rate', [(1, 5, 0.5), (2, 10, 0.0), (3, 20, 0.7)])
-def test_build_mlp(n_layers, hidden_size, dropout_rate):
-    model = build_mlp(10, 2, n_layers, hidden_size, dropout_rate)
-    assert len(model) == 3 * n_layers + 3 
-    # Check dropout p in hidden layers
-    for i in range(2, len(model), 3):  # Dropout positions
-        assert isinstance(model[i], nn.Dropout)
-        assert model[i].p == dropout_rate
+@pytest.mark.parametrize('n_layers', [1, 2])
+def test_build_mlp(n_layers):
+    """
+    Test if the neural network is built properly
+    """
+    input_size = 10
+    output_size = 2
+    hidden_size = 5
+    model = build_mlp(input_size, output_size, n_layers, hidden_size, dropout_rate=0.5)
+    assert len(model) == 3 * (n_layers + 1)
+
+    assert type(model[0]) is nn.Linear
+    assert model[0].in_features == input_size
+    assert model[0].out_features == hidden_size
+    assert type(model[1]) == nn.ReLU
+    assert type(model[3]) == nn.Dropout
+    assert model[-1].out_features == output_size
 
 
 def test_mlp_inference():
@@ -24,4 +33,3 @@ def test_mlp_inference():
     data = np.random.rand(n_examples, 10)
     output = model.predict(data)
     assert output.shape[0] == n_examples
-
